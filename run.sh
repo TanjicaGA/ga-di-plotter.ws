86a52012fbadaf87ef98d2caa3b1f885b0a75031
TanjicaGA <totorowalking@gmail.com>
2023-05-26 15:35:13 +0200

    Rscript for windows

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ WATCHED_DIR=${1-./}
 PORT=${2-5000}
 
 start () {
-  R --slave -e "shiny::runApp('$WATCHED_DIR', port = $PORT, host='0.0.0.0')"
+  "/cygdrive/c/Program Files/R/R-4.2.1/bin/R.exe" --slave -e "shiny::runApp('$WATCHED_DIR', port = $PORT, host='0.0.0.0')"
   PID=$!
 }
 
